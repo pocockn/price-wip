e5bf46d17c615f56329c82032f235ad48dd3edc5
Nick Pocock <pocockn@hotmail.co.uk>
2022-08-10 12:12:39 +0200

    Price calculations with gas price

--- calculator/ethereum_calculator.js
+++ calculator/ethereum_calculator.js
@@ -1,40 +1,34 @@
-// node connection
-const { BigNumber, ethers } = require("ethers");
-// node api
+const { BigNumber, ethers } = require('ethers');
 const Web3 = require('web3');
-
-const web3 = new Web3("https://cloudflare-eth.com")
+const web3 = new Web3('https://cloudflare-eth.com');
 
 // swapAmount: wei
 class EthereumCalculator {
-  constructor(routerABI, routerAddress, swapFrom, swapTo, swapAmount, swapToDecimal) {
-    this.routerABI = routerABI;
-    this.routerAddress = routerAddress;
-    this.swapFrom = swapFrom;
-    this.swapToDecimal = swapToDecimal
-    this.swapTo = swapTo;
-    this.swapAmount = swapAmount;
-    this.routerContract = new web3.eth.Contract(
-      this.routerABI,
-      this.routerAddress,
-    );
-  }
+	constructor(routerABI, routerAddress, swapFrom, swapTo, swapAmount, swapToDecimal) {
+		this.routerABI = routerABI;
+		this.routerAddress = routerAddress;
+		this.swapFrom = swapFrom;
+		this.swapToDecimal = swapToDecimal;
+		this.swapTo = swapTo;
+		this.swapAmount = swapAmount;
+		this.routerContract = new web3.eth.Contract(this.routerABI, this.routerAddress);
+	}
 
-  async getSellPrice() {
-    const pathToSell = await this.routerContract.methods.getAmountsOut(
-      this.swapAmount,
-      [ this.swapFrom, this.swapTo ],
-    ).call();
-    return web3.utils.fromWei(pathToSell[pathToSell.length - 1], 'ether') * 100/ 100;
-  }
+	async getSellPrice() {
+		const pathToSell = await this.routerContract.methods
+			.getAmountsOut(this.swapAmount, [this.swapFrom, this.swapTo])
+			.call();
+		return (web3.utils.fromWei(pathToSell[pathToSell.length - 1], 'ether') * 100) / 100;
+	}
 
-  async getBuyPrice() {
-    const pathToBuy = await this.routerContract.methods.getAmountsIn(
-      this.swapAmount,
-      [ this.swapTo, this.swapFrom ],
-    ).call();
-    return Math.floor(ethers.utils.parseUnits(pathToBuy[0], this.swapToDecimal) * 100) / 100
-  }
+	async getBuyPrice() {
+		const pathToBuy = await this.routerContract.methods
+			// getAmountsIn already includes Uniswap & SushiSwap fees in the calculation
+			.getAmountsIn(this.swapAmount, [this.swapTo, this.swapFrom])
+			.call();
+		// TODO: modify the / 1e12 on a per token basis
+		return Math.floor(ethers.utils.parseUnits(pathToBuy[0], this.swapToDecimal) * 100) / 100 / 1e12;
+	}
 }
 
 module.exports = EthereumCalculator;
--- src/ethereum.js
+++ src/ethereum.js
@@ -1,25 +1,64 @@
 const EthereumCalculator = require('../calculator/ethereum_calculator');
 const { tokens, routers, ABIs, swapFrom } = require('../addresses/ethereum');
+const Web3 = require('web3');
+const web3 = new Web3('https://cloudflare-eth.com');
 
-const main = async () => {
-  // user input of 2 tokens
-  const swapAmount = '1000000000000000000'; // in wei = 1 ethereum
+async function estimateProfitAfterTradingFees(uniswapPrice, sushiswapPrice) {
+	if (uniswapPrice > sushiswapPrice) {
+		return Math.abs(sushiswapPrice - uniswapPrice);
+	}
 
-  console.log("Uniswap:")
-  await Promise.all(tokens.map(async (token) => {
-    const uniswapV2EthCalc = new EthereumCalculator(ABIs.uniswapV2ABI, routers.uniswapV2, swapFrom.WETH, token.address, swapAmount, token.decimal);
-    const uniswapV2V2BuyPrice = await uniswapV2EthCalc.getBuyPrice();
-    console.log("WETH", "/", token.name, " ", uniswapV2V2BuyPrice)
-  }));
-
-  console.log("--------")
-
-  console.log("SushiSwap:")
-  await Promise.all(tokens.map(async (token) => {
-    const sushiSwapEthCalc = new EthereumCalculator(ABIs.uniswapV2ABI, routers.sushiswapV2, swapFrom.WETH, token.address, swapAmount, token.decimal);
-    const sushiSwapV2V2BuyPrice = await sushiSwapEthCalc.getBuyPrice();
-    console.log("WETH", "/", token.name, " ", sushiSwapV2V2BuyPrice)
-  }));
+	console.log(`trade is not profitable from SushiSwap to Uniswap`);
 }
 
-main().catch(error => console.log(error));
+const main = async () => {
+	//listening for incoming new blocks
+	const newBlockEvent = web3.eth.subscribe('newBlockHeaders');
+
+	newBlockEvent.on('connected', () => {
+		console.log('\nBot listening!\n');
+	});
+
+	// user input of 2 tokens
+	const swapAmount = '1000000000000000000'; // in wei = 1 ethereum
+
+	await Promise.all(
+		tokens.map(async (token) => {
+			const uniswapV2EthCalc = new EthereumCalculator(
+				ABIs.uniswapV2ABI,
+				routers.uniswapV2,
+				swapFrom.WETH,
+				token.address,
+				swapAmount,
+				token.decimal,
+			);
+			const uniswapV2V2BuyPrice = await uniswapV2EthCalc.getBuyPrice();
+			console.log('Uniswap:');
+			console.log('WETH', '/', token.name, ' ', uniswapV2V2BuyPrice);
+
+			const sushiSwapEthCalc = new EthereumCalculator(
+				ABIs.uniswapV2ABI,
+				routers.sushiswapV2,
+				swapFrom.WETH,
+				token.address,
+				swapAmount,
+				token.decimal,
+			);
+			const sushiSwapV2V2BuyPrice = await sushiSwapEthCalc.getBuyPrice();
+			console.log('Sushi Swap: ');
+			console.log('WETH', '/', token.name, ' ', sushiSwapV2V2BuyPrice);
+
+			let estimatedProfit = await estimateProfitAfterTradingFees(uniswapV2V2BuyPrice, sushiSwapV2V2BuyPrice);
+			console.log('estimated profit on trade ', 'WETH/', token.name, ' ', estimatedProfit);
+
+			// const gasLimit = 100000;
+			let gasPrice;
+			await web3.eth.getGasPrice().then((result) => {
+				gasPrice = web3.utils.fromWei(result, 'ether');
+			});
+			console.log('gas price in Eth ', gasPrice);
+		}),
+	);
+};
+
+main();
